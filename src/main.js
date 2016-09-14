8a47cad1f01c75905912d140873d697c8f3cfbfe
Perrine Brunel <perrine.brunel@gmail.com>
2016-09-14 11:13:17 +0200

    Fix learn with last timestamp for decision tree

--- src/main.js
+++ src/main.js
@@ -60,14 +60,14 @@ fetch('http://craft.ai/content/data/twor_ROOM_R1.json')
     return CRAFT_CLIENT.getAgent('ROOM_R1')
     .then(function() {
       console.log('Operations successfully added to agent ROOM_R1');
-      return CRAFT_CLIENT.getAgentInspectorUrl('ROOM_R1', 1272745200);
+      return CRAFT_CLIENT.getAgentInspectorUrl('ROOM_R1', context[context.length - 1].timestamp); // tree for the last timestamp pushed
     });
   })
   .then(function(url) {
     console.log('Agent ROOM_R1 inspectable at https://beta.craft.ai/inspector.');
     // 4 - Get some decisions
     // Download the tree
-    return CRAFT_CLIENT.getAgentDecisionTree('ROOM_R1', 1272745200);
+    return CRAFT_CLIENT.getAgentDecisionTree('ROOM_R1', context[context.length - 1].timestamp);
   })
   .then(function(tree) {
     console.log('Tree downloaded !');
