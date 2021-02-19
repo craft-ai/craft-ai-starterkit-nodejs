844d0d19c8335dc05be82f646aeeddf32a015253
yassineSaid-craft <78745138+yassineSaid-craft@users.noreply.github.com>
2021-02-19 15:19:13 +0100

    Update src/main.js
    Co-authored-by: Brunel Perrine <perrine.brunel@gmail.com>

--- src/main.js
+++ src/main.js
@@ -97,7 +97,6 @@ utils.readData(LOCAL_FILE_PATH1)
           new craftai.Time('2010-01-04T09:42:00+09:00')
         );
         console.log(`- The light is ${d4.output.light.predicted_value} when there is some movement at 9:42AM.`);
-        {
           const d5 = craftai.interpreter.decide(
             tree,
             {
